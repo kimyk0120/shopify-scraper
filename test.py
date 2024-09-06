a0f81e86c9ba05ae3bba043e462165b74ba3aceb
kimyk0120 <kimyk0120@gmail.com>
2024-09-07 08:43:39 +0900

    Initial commit

---
+++ test.py
@@ -0,0 +1,70 @@
+import os
+import sys
+import csv
+import json
+import time
+import urllib.request
+from urllib.error import HTTPError
+from optparse import OptionParser
+import ssl
+from bs4 import BeautifulSoup
+
+ssl_context = ssl.create_default_context()
+ssl_context.check_hostname = False
+ssl_context.verify_mode = ssl.CERT_NONE
+
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+
+end_page = 21  # 페이지가 어디까지 인지  확인 필요
+csv_file_path = 'products_add_image_url.csv'
+
+# Check if the file exists
+file_exists = os.path.isfile(csv_file_path)
+
+with open(csv_file_path, 'a', newline='') as csv_file:
+    csv_writer = csv.writer(csv_file)
+
+    # If the file does not exist, write the header
+    if not file_exists:
+        csv_writer.writerow(['Title', 'Description', 'Specs', 'Price' , "Images"])
+
+    for page in range(1, end_page+1):
+        url = "https://www.chrononation.com/collections/shop/products.json?page={}".format(page)
+
+        req = urllib.request.Request(
+            url,
+            data=None,
+            headers={
+                'User-Agent': USER_AGENT
+            }
+        )
+
+        data = urllib.request.urlopen(req, context=ssl_context).read()
+        products = json.loads(data.decode())['products']
+
+        for product in products:
+            title = product['title']
+            description_body = product['body_html']
+
+            soup = BeautifulSoup(description_body, 'html.parser')
+            description = soup.find("p").get_text()
+            description = description.replace('\n', '').replace('\t', '')
+
+            specs_tr = soup.find_all("tr")
+            spec_json = {}
+            for spec in specs_tr:
+                key = spec.find_all("td")[0].get_text()
+                value = spec.find_all("td")[1].get_text()
+                spec_json[key] = value
+
+            specs = json.dumps(spec_json)
+            price = product['variants'][0]['price']
+
+            images = product['images']
+            image_urls = []
+            for image in images:
+                image_urls.append(image['src'])
+
+            csv_writer.writerow([title, description, specs, price, image_urls])
+
+print("end of script")
